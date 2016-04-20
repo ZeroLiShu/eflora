829df9b94d3181f8d72d7b74c0fa8a1793b91cd2
Li Shu <ZeroLiShu@users.noreply.github.com>
2016-04-20 15:03:47 +0800

    Update client.py

--- plant/client.py
+++ plant/client.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import requests
+import json
 
 from .common import *
 
@@ -11,7 +12,30 @@ class EfloraClient:
         self._session = requests.Session()
         self._session.headers.update(Default_Header)
 
+    def _get_plant_list_from_page(self, name, pageindex):
+        plantlist = []
+        
+        pageurl = Eflora_Search_URL + '/' + name + '?page=' + str(pageindex)
+        r = self._session.get(pageurl)
+        soup = BeautifulSoup(r.content)
+        
+        table = soup.find('div', cellSpacing='0')
+        for tr in table.find_all('tr', style=True):
+            plantdict = {}
+            tdlist = tr.find_all('td')
+            assert 5 == len(tdlist)
+            plantdict['name'] = tdlist[0].text
+            plantdict['latin'] = tdlist[1].text
+            plantdict['alias'] = tdlist[2].text
+            plantdict['slug'] = tdlist[3].text
+            plantdict['url'] = Eflora_URL + tdlist[4]['href']
+            plantlist.append(plantdict)
+        
+        return plantlist
+
     def search(self, name):
+        plantlist = []
+        
         pageurl = Eflora_Search_URL + '/' + name + '?page=1'
         r = self._session.get(pageurl)
         soup = BeautifulSoup(r.content)
@@ -19,12 +43,7 @@ class EfloraClient:
         alist = divpage.find_all('a')
         pagenum = int(alist[len(alist) - 2].text)
         
-        table = soup.find('div', cellSpacing='0')
-        for tr in table.find_all('tr', style=True):
-            tdlist = tr.find_all('td')
-            assert 5 == len(tdlist)
-            name = tdlist[0].text
-            latin = tdlist[1].text
-            alias = tdlist[2].text
-            slug = tdlist[3].text
-            url = Eflora_URL + tdlist[4]['href']
+        for pageindex in range(1, pagenum + 1):
+            plantlist.extend(_get_plant_list_from_page(name, pageindex))
+        
+        return json.dumps(plantlist)
