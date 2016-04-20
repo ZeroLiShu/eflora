4dc82220babc5d06b5f09380d12a8e89b68a5ea1
Li Shu <ZeroLiShu@users.noreply.github.com>
2016-04-20 15:17:01 +0800

    Update plant.py

--- plant/plant.py
+++ plant/plant.py
@@ -5,6 +5,29 @@ from .base import BaseEflora
 
 class Plant(BaseEflora):
 
-    def __init__(self, url, session=None):
+    def __init__(self, url, name=None, latin=None, alias=None, slug=None, session=None):
         self.url = url
         self._session = session
+        self._name = name
+        self._latin = name
+        self._alias = alias
+        self._slug = slug
+    
+    def position(self):
+        pass
+    
+    def relatives(self):
+        pass
+    
+    def fullname(self):
+        pass
+    
+    def description(self):
+        pass
+    
+    def indextable(self):
+        pass
+    
+    def subclass(self):
+        pass
+
