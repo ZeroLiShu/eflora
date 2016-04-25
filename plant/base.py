f51f1f0442718b386b6dba85ac9e0bd371e26b71
Li Shu <ZeroLiShu@users.noreply.github.com>
2016-04-25 10:05:24 +0800

    Update base.py

--- plant/base.py
+++ plant/base.py
@@ -3,14 +3,14 @@ from requests import Response
 
 class BaseEflora:
     def _gen_soup(self, content):
-        self.soup = BeautifulSoup(content)
+        self._soup = BeautifulSoup(content)
 
     def _get_content(self):
         resp = self._session.get(self.url[:-1])
         return resp.content
 
     def _make_soup(self):
-        if self.url and not self.soup:
+        if self._url and not self._soup:
             self._gen_soup(self._get_content())
 
     def refresh(self):
